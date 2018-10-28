256eadb52a2eced1cfc4f2f3baefe378d762ac52
Robert Lee <32447910+robertklee@users.noreply.github.com>
2018-10-28 15:35:57 -0700

    add lab 1 part 2 WORKING code

--- main.c
+++ main.c
@@ -46,174 +46,174 @@ void myTIM2_Init(void);
 void myEXTI_Init(void);
 
 volatile unsigned char previousEdgeFound = 0;  // 0/1: first/not first edge
-volatile unsigned char stopPrinting = 0;
 
 int
 main(int argc, char* argv[]){
 
 
-	trace_printf("This is Part 2 of Introductory Lab...\n");
-	trace_printf("System clock: %u Hz\n", SystemCoreClock);
+    trace_printf("This is Part 2 of Introductory Lab...\n");
+    trace_printf("System clock: %u Hz\n", SystemCoreClock);
 
-	myGPIOA_Init();		/* Initialize I/O port PA */
-	myTIM2_Init();		/* Initialize timer TIM2 */
-	myEXTI_Init();		/* Initialize EXTI */
+    myGPIOA_Init();     /* Initialize I/O port PA */
+    myTIM2_Init();      /* Initialize timer TIM2 */
+    myEXTI_Init();      /* Initialize EXTI */
 
 
 
-	while (1)
-	{
-		//trace_printf("counter: %d\n", TIM2->CNT);
-	}
+    while (1)
+    {
 
-	return 0;
+    }
+
+    return 0;
 
 }
 
 
 void myGPIOA_Init()
 {
-	/* Enable clock for GPIOA peripheral */
-	// Relevant register: RCC->AHBENR
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+    /* Enable clock for GPIOA peripheral */
+    // Relevant register: RCC->AHBENR
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 
-	/* Configure PA1 as input */
-	// Relevant register: GPIOA->MODER
-	GPIOA->MODER &= ~(GPIO_MODER_MODER1);
+    /* Configure PA1 as input */
+    // Relevant register: GPIOA->MODER
+    GPIOA->MODER &= ~(GPIO_MODER_MODER1);
 
-	/* Ensure no pull-up/pull-down for PA1 */
-	// Relevant register: GPIOA->PUPDR
-	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR1);
+    /* Ensure no pull-up/pull-down for PA1 */
+    // Relevant register: GPIOA->PUPDR
+    GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR1);
 }
 
 
 void myTIM2_Init()
 {
-	/* Enable clock for TIM2 peripheral */
-	// Relevant register: RCC->APB1ENR
-	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+    /* Enable clock for TIM2 peripheral */
+    // Relevant register: RCC->APB1ENR
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 
-	/* Configure TIM2: buffer auto-reload, count up, stop on overflow,
-	 * enable update events, interrupt on overflow only */
-	// Relevant register: TIM2->CR1
-	TIM2->CR1 = ((uint16_t)0x008C); /* 008C sets auto-reload preload enabled, stops the
-									 *	counter on an update event, and counter ONLY
-									 *	controlled by over.underflow */
+    /* Configure TIM2: buffer auto-reload, count up, stop on overflow,
+     * enable update events, interrupt on overflow only */
+    // Relevant register: TIM2->CR1
+    TIM2->CR1 = ((uint16_t)0x008C); /* 008C sets auto-reload preload enabled, stops the
+                                     *  counter on an update event, and counter ONLY
+                                     *  controlled by over.underflow */
 
-	/* Set clock prescaler value */
-	TIM2->PSC = myTIM2_PRESCALER;
-	/* Set auto-reloaded delay */
-	TIM2->ARR = myTIM2_PERIOD;
+    /* Set clock prescaler value */
+    TIM2->PSC = myTIM2_PRESCALER;
+    /* Set auto-reloaded delay */
+    TIM2->ARR = myTIM2_PERIOD;
 
-	/* Update timer registers */
-	// Relevant register: TIM2->EGR
-	TIM2->EGR =((uint16_t)0x0001);
+    /* Update timer registers */
+    // Relevant register: TIM2->EGR
+    TIM2->EGR =((uint16_t)0x0001);
 
-	/* Assign TIM2 interrupt priority = 0 in NVIC */
-	// Relevant register: NVIC->IP[3], or use NVIC_SetPriority
-	NVIC_SetPriority(TIM2_IRQn, 64); //TODO could be better as 64?
+    /* Assign TIM2 interrupt priority = 0 in NVIC */
+    // Relevant register: NVIC->IP[3], or use NVIC_SetPriority
+    NVIC_SetPriority(TIM2_IRQn, 64); //TODO could be better as 64?
 
-	/* Enable TIM2 interrupts in NVIC */
-	// Relevant register: NVIC->ISER[0], or use NVIC_EnableIRQ
-	NVIC_EnableIRQ(TIM2_IRQn);
+    /* Enable TIM2 interrupts in NVIC */
+    // Relevant register: NVIC->ISER[0], or use NVIC_EnableIRQ
+    NVIC_EnableIRQ(TIM2_IRQn);
 
-	/* Enable update interrupt generation */
-	// Relevant register: TIM2->DIER
-	TIM2->DIER |= TIM_DIER_UIE;
+    /* Enable update interrupt generation */
+    // Relevant register: TIM2->DIER
+    TIM2->DIER |= TIM_DIER_UIE;
 }
 
 
 void myEXTI_Init()
 {
-	/* Map EXTI1 line to PA1 */
-	// Relevant register: SYSCFG->EXTICR[0]
-	SYSCFG->EXTICR[0] &= ((uint32_t) 0xFFFFFF0F);
+    /* Map EXTI1 line to PA1 */
+    // Relevant register: SYSCFG->EXTICR[0]
+    SYSCFG->EXTICR[0] &= ((uint32_t) 0xFFFFFF0F);
 
-	/* EXTI1 line interrupts: set rising-edge trigger */
-	// Relevant register: EXTI->RTSR
-	EXTI->RTSR |= ((uint32_t) 0x00000002);
+    /* EXTI1 line interrupts: set rising-edge trigger */
+    // Relevant register: EXTI->RTSR
+    EXTI->RTSR |= ((uint32_t) 0x00000002);
 
-	/* Unmask interrupts from EXTI1 line */
-	// Relevant register: EXTI->IMR
-	EXTI->IMR |= ((uint32_t) 0x00000002);
+    /* Unmask interrupts from EXTI1 line */
+    // Relevant register: EXTI->IMR
+    EXTI->IMR |= ((uint32_t) 0x00000002);
 
-	/* Assign EXTI1 interrupt priority = 0 in NVIC */
-	// Relevant register: NVIC->IP[1], or use NVIC_SetPriority
-	NVIC_SetPriority(EXTI0_1_IRQn, 0);
+    /* Assign EXTI1 interrupt priority = 0 in NVIC */
+    // Relevant register: NVIC->IP[1], or use NVIC_SetPriority
+    NVIC_SetPriority(EXTI0_1_IRQn, 0);
 
 
-	/* Enable EXTI1 interrupts in NVIC */
-	// Relevant register: NVIC->ISER[0], or use NVIC_EnableIRQ
-	NVIC_EnableIRQ(EXTI0_1_IRQn);
+    /* Enable EXTI1 interrupts in NVIC */
+    // Relevant register: NVIC->ISER[0], or use NVIC_EnableIRQ
+    NVIC_EnableIRQ(EXTI0_1_IRQn);
 }
 
 
 /* This handler is declared in system/src/cmsis/vectors_stm32f0xx.c */
 void TIM2_IRQHandler()
 {
-	/* Check if update interrupt flag is indeed set */
-	if ((TIM2->SR & TIM_SR_UIF) != 0)
-	{
-		trace_printf("\n*** Overflow! ***\n");
+    /* Check if update interrupt flag is indeed set */
+    if ((TIM2->SR & TIM_SR_UIF) != 0)
+    {
+        trace_printf("\n*** Overflow! ***\n");
 
-		/* Clear update interrupt flag */
-		// Relevant register: TIM2->SR
-		TIM2->SR &= ~(TIM_SR_UIF);
+        /* Clear update interrupt flag */
+        // Relevant register: TIM2->SR
+        TIM2->SR &= ~(TIM_SR_UIF);
 
-		/* Restart stopped timer */
-		// Relevant register: TIM2->CR1
-		TIM2->CR1 |= TIM_CR1_CEN;
-	}
+        /* Restart stopped timer */
+        // Relevant register: TIM2->CR1
+        TIM2->CR1 |= TIM_CR1_CEN;
+    }
 }
 
 
 /* This handler is declared in system/src/cmsis/vectors_stm32f0xx.c */
 void EXTI0_1_IRQHandler()
 {
-	// Your local variables...
-	volatile unsigned int frequency = 0;
-	volatile unsigned int counterValue = 0;
-	/* Check if EXTI1 interrupt pending flag is indeed set */
-	if ((EXTI->PR & EXTI_PR_PR1) != 0)
-	{
-		//
-		// 1. If this is the first edge:
-		//	- Clear count register (TIM2->CNT).
-		//	- Start timer (TIM2->CR1).
-		//    Else (this is the second edge):
-		//	- Stop timer (TIM2->CR1).
-		//	- Read out count register (TIM2->CNT).
-		//	- Calculate signal period and frequency.
-		//	- Print calculated values to the console.
-		//	  NOTE: Function trace_printf does not work
-		//	  with floating-point numbers: you must use
-		//	  "unsigned int" type to print your signal
-		//	  period and frequency.
-		//
-		// 2. Clear EXTI1 interrupt pending flag (EXTI->PR).
-		//
+    // Your local variables...
+    volatile unsigned int frequency = 0;
+    volatile unsigned int period = 0;
+    volatile unsigned int counterValue = 0;
+    /* Check if EXTI1 interrupt pending flag is indeed set */
+    if ((EXTI->PR & EXTI_PR_PR1) != 0)
+    {
+        //
+        // 1. If this is the first edge:
+        //  - Clear count register (TIM2->CNT).
+        //  - Start timer (TIM2->CR1).
+        //    Else (this is the second edge):
+        //  - Stop timer (TIM2->CR1).
+        //  - Read out count register (TIM2->CNT).
+        //  - Calculate signal period and frequency.
+        //  - Print calculated values to the console.
+        //    NOTE: Function trace_printf does not work
+        //    with floating-point numbers: you must use
+        //    "unsigned int" type to print your signal
+        //    period and frequency.
+        //
+        // 2. Clear EXTI1 interrupt pending flag (EXTI->PR).
+        //
 
-		if (!previousEdgeFound /* && !stopPrinting*/) {
-			TIM2->CNT = ((uint32_t) 0x0);
-			TIM2->CR1 |= TIM_CR1_CEN;
-			previousEdgeFound = 1;
-		}
+        if (!previousEdgeFound) {
+            TIM2->CNT = ((uint32_t) 0x0);
+            TIM2->CR1 |= TIM_CR1_CEN;
+            previousEdgeFound = 1;
+        }
 
-		else
-		{
-			TIM2->CR1 &= ~(TIM_CR1_CEN);
-			counterValue = TIM2->CNT;
-			frequency = ((48000000000)/(counterValue)); // extra factor of 1000 for mHz
-//			trace_printf("Counter Value: %d \n", (unsigned int) counterValue);
-			trace_printf("Signal Frequency: %d mHz\n", (unsigned int) frequency);
-//			trace_printf("Signal Period: 1/ %d \n", (unsigned int) frequency);
-			TIM2->CNT = ((uint32_t) 0x0);
-			previousEdgeFound = 0;
-			//stopPrinting = 1;
-//			TIM2->CR1 |= (TIM_CR1_CEN);
-		}
-		EXTI->PR |= (EXTI_PR_PR1); // to clear, software must write 1
-	}
+        else
+        {
+            TIM2->CR1 &= ~(TIM_CR1_CEN);
+            EXTI->IMR &= ~((uint32_t) 0x00000002);
+            counterValue = TIM2->CNT;
+            frequency = (unsigned int)((48000000000.0)/(counterValue)); // extra factor of 1000 for mHz
+            trace_printf("Signal Frequency: %d.%03d Hz\t", frequency/1000, frequency%1000);
+            period = 1000000000.0 / frequency; //period in ms
+            trace_printf("Signal Period: %d.%06d s \n", period/1000000, period%1000000);
+            TIM2->CNT = ((uint32_t) 0x0);
+            previousEdgeFound = 0;
+            EXTI->IMR |= ((uint32_t) 0x00000002);
+        }
+        EXTI->PR |= (EXTI_PR_PR1); // to clear, software must write 1
+    }
 }
 
 
